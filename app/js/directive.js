e9fa248cf6d0e78048c9f69bc43c1b6769f31249
aqingsao <vinci.zhang@gmail.com>
2013-08-26 01:22:13 +0800

    add directive example

--- app/js/directive.js
+++ app/js/directive.js
@@ -1,8 +1,33 @@
 var app = angular.module("superhero", [])
 
-app.directive("superman", function(){
+app.directive("supermane", function(){
   return {
     restrict: "E",
-    template: "<div>Here I am to save the day</div>"
+    template: "<div>I'm superman-e</div>"
+  }
+});
+
+app.directive("supermana", function(){
+  return {
+    restrict: "A",
+    link: function(){
+      alert("I'm supermanA")
+    }
+  }
+});
+app.directive("supermanC", function(){
+  return {
+    restrict: "C",
+    link: function(){
+      alert("I'm supermanC")
+    }
+  }
+});
+app.directive("supermanm", function(){
+  return {
+    restrict: "M",
+    link: function(){
+      alert("I'm supermanM")
+    }
   }
 });
