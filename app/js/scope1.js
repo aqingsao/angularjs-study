a0cd9856ac3c579c7a2228c065bf064e6dcc7d92
aqingsao <vinci.zhang@gmail.com>
2013-08-29 23:11:17 +0800

    add more examples for scope usage

--- app/js/scope1.js
+++ app/js/scope1.js
@@ -14,24 +14,3 @@ app.directive("kid2", function(){
     template: '<input type="text" ng-model="chore""> {{chore}}'
   }
 });
-
-app.controller("ChoreCtrl", function($scope){
-  $scope.logChore = function(chore){
-    alert(chore + " is done!")
-  }
-})
-app.directive("kid3", function(){
-  return {
-    restrict: 'E',
-    scope: {
-      done: "&"
-    },
-    template: '<input type="text" ng-model="chore""> ' +
-      '{{chore}} ' +
-      '<div class="button" ng-click="done(\'{{chore}}\')">I\'m done</div>',
-    link: function(scope){
-      console.log(scope);
-    }
-  }
-});
-
--- app/js/scope2.js
+++ app/js/scope2.js
@@ -1,6 +1,26 @@
 var app = angular.module("app", []);
 
-app.controller("AppCtrl", function($scope){
+app.controller("AppCtrl1", function($scope){
+  $scope.logChore = function(chore){
+    alert(chore + " is done!")
+  }
+})
+app.directive("kid", function(){
+  return {
+    restrict: 'E',
+    scope: {
+      done: "&"
+    },
+    template: '<input type="text" ng-model="chore""> ' +
+      '{{chore}} ' +
+      '<div class="button" ng-click="done({chore:chore})">I\'m done</div>',
+    link: function(scope){
+      console.log(scope);
+    }
+  }
+});
+
+app.controller("AppCtrl2", function($scope){
   $scope.ctrlFlavor = "blackBerry";
 })
 app.directive("drink", function(){
@@ -11,13 +31,30 @@ app.directive("drink", function(){
     template: '<div>{{flavor}}</div>'
   }
 });
-// equals to:
-//app.directive("drink", function(){
-//  return {
-//    scope: {},
-//    template: '<div>{{flavor}}</div>',
-//    link: function(scope, element, attrs){
-//      scope.flavor = attrs.flavor
-//    }
-//  }
-//});
+
+app.controller("AppCtrl3", function($scope){
+  $scope.ctrlFlavor = "redBerry";
+})
+app.directive("eat", function(){
+  return {
+    scope: {
+      flavor: "="
+    },
+    template: '<input type="text" ng-model="flavor">'
+  }
+})
+
+app.controller("AppCtrl4", function($scope){
+  $scope.callHome = function(message){
+    alert(message);
+  }
+})
+app.directive("phone", function(){
+  return {
+    scope: {
+      dial: "&"
+    },
+    template: '<input type="text" ng-model="value">' +
+      '<div class="button" ng-click="dial({message:value})">call home!</div>'
+  }
+})
