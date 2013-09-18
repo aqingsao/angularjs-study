256210e065a466333229f36c240f49cef182e99b
aqingsao <vinci.zhang@gmail.com>
2013-09-18 11:43:45 +0800

    refactor a bit

--- app/js/zippy.js
+++ app/js/zippy.js
@@ -1,14 +1,11 @@
 var app = angular.module('app', []);
-app.controller("AppCtrl", function ($scope) {
-
-});
 
 app.directive("zippy", function () {
   return {
     restrict: "E",
     scope: {title: "@"},
     transclude: true,
-    template: '<div><h3 ng-click="toggleContent()">This is {{title}}</h3><div ng-show="isContentVisible" ng-transclude>Hi, </div></div>',
+    template: '<div><h3 ng-click="toggleContent()">This is {{title}} <span style="font-size: 60%; font-weight: normal; color: grey;">click to show content</span></h3><div ng-show="isContentVisible" ng-transclude>Hi, </div></div>',
     link: function (scope) {
       scope.isContentVisible = false;
       scope.toggleContent = function () {
